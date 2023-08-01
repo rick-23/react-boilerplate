fedba2b44223c5c57312716b41c2aeef9a90648d
rick-23 <74650400+rick-23@users.noreply.github.com>
2023-08-01 14:40:23 -0400

    Updated dependencies to latest

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # react-boilerplate
 React template with Webpack, Babel and ESLint configured from scratch
+
+Use this template for creating UI projects in React
--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 const webpack = require('webpack');
  
 module.exports = {
@@ -8,9 +9,14 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js)$/,
+        test: /\.?js$/,
         exclude: /node_modules/,
-        use: ['babel-loader', "eslint-loader"]
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        },
       }
     ]
   },
@@ -20,16 +26,19 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Hello Webpack bundled JavaScript Project',
+      title: 'Webpack bundled, Bable tanspiled React Project',
       template: path.resolve(__dirname, '..',  './src/index.html'),
     }),
     new webpack.HotModuleReplacementPlugin(),
+    new ESLintPlugin({extensions: ['js', 'jsx']})
   ],
   output: {
     path: path.resolve(__dirname, '..', './dist'),
     filename: 'bundle.js'
   },
   devServer: {
-    contentBase: path.resolve(__dirname, '..', './dist'),
+    static: {
+      directory: path.resolve(__dirname, '..', './dist'),
+    },
   },
 };
